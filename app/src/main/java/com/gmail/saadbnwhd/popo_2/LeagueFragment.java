5fe83ded5a2a2705eb1837f80fbbb7c3b1c61d44
MusabAkram <musabakram16@gmail.com>
2016-12-12 21:56:50 +0500

    league added

---
+++ app/src/main/java/com/gmail/saadbnwhd/popo_2/LeagueFragment.java
@@ -0,0 +1,38 @@
+package com.gmail.saadbnwhd.popo_2;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * Created by Musab on 12/6/2016.
+ */
+public class LeagueFragment extends Fragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View view= inflater.inflate(R.layout.fragment_league, container, false);
+        Button button = (Button) view.findViewById(R.id.teams);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent Team=new Intent("android.intent.action.Teams");
+                startActivity(Team);
+            }
+        });
+        return view;
+    }
+}
--- app/src/main/java/com/gmail/saadbnwhd/popo_2/PlayersFragment.java
+++ app/src/main/java/com/gmail/saadbnwhd/popo_2/PlayersFragment.java
@@ -1,40 +1,26 @@
 package com.gmail.saadbnwhd.popo_2;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
-
 /**
- * Created by Musab on 12/6/2016.
+ * Created by Musab on 12/12/2016.
  */
-public class PlayersFragment extends Fragment{
+public class PlayersFragment extends Fragment {
+    public PlayersFragment() {
+
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-       View view= inflater.inflate(R.layout.fragment_players, container, false);
-        Button button = (Button) view.findViewById(R.id.teams);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent Team=new Intent("android.intent.action.Teams");
-                startActivity(Team);
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_players, container, false);
     }
 }
