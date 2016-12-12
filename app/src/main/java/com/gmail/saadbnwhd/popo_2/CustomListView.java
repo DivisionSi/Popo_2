59c38433160c48c311d518ac3c06e577ad40399b
DivisionSi <saadbnwhd@gmail.com>
2016-12-12 21:50:31 +0500

    Getting Teams from DB

--- app/src/main/java/com/gmail/saadbnwhd/popo_2/CustomListView.java
+++ app/src/main/java/com/gmail/saadbnwhd/popo_2/CustomListView.java
@@ -8,30 +8,37 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by Musab on 12/11/2016.
  */
 public class CustomListView extends ArrayAdapter<String> {
     private final Activity context;
-    private final String[] teams;
+    private final ArrayList<String> teams;
+    private final ArrayList<String> locations;
     private final Integer[] imgid;
 
-    public CustomListView(Activity context, String[] teams, Integer[] imgid) {
+    public CustomListView(Activity context, ArrayList<String> teams, ArrayList<String> locations, Integer[] imgid) {
         super(context, R.layout.listview, teams);
         // TODO Auto-generated constructor stub
 
         this.context=context;
         this.teams=teams;
+        this.locations=locations;
         this.imgid=imgid;
     }
 
     public View getView(int position,View view,ViewGroup parent) {
         LayoutInflater inflater=context.getLayoutInflater();
-        View rowView=inflater.inflate(R.layout.listview, null,true);
+        View rowView=inflater.inflate(R.layout.listview, null, true);
 
         TextView txtTitle = (TextView) rowView.findViewById(R.id.team);
+        TextView txtlocation = (TextView) rowView.findViewById(R.id.location);
+
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
-        txtTitle.setText(teams[position]);
+        txtTitle.setText(teams.get(position));
+        txtlocation.setText(locations.get(position));
         imageView.setImageResource(imgid[position]);
 
         return rowView;
--- app/src/main/java/com/gmail/saadbnwhd/popo_2/Teams.java
+++ app/src/main/java/com/gmail/saadbnwhd/popo_2/Teams.java
@@ -1,25 +1,33 @@
 package com.gmail.saadbnwhd.popo_2;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.ArrayList;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 
 public class Teams extends AppCompatActivity{
     ListView list;
     Firebase ref; //Reference to our DB
-    String[] teams = {
-            "POPO",
-            "BARCELONA",
-            "REAL MADRID",
-    };
+
+
+    ArrayList<String> teams = new ArrayList<String>(); //String array for Team Names
+    ArrayList<String> locations = new ArrayList<String>(); //String array for Team Locations
 
     Integer[] imgid = {
+            R.drawable.logo,
+            R.drawable.logo,
+            R.drawable.logo,
             R.drawable.logo,
             R.drawable.logo,
             R.drawable.logo,
@@ -31,20 +39,74 @@ public class Teams extends AppCompatActivity{
         setContentView(R.layout.activity_team);
         Firebase.setAndroidContext(this);  //Setting up Firebase
         ref=new Firebase("https://poponfa-8a11a.firebaseio.com/");
-        CustomListView adapter = new CustomListView(this, teams, imgid);
+
+
+
+
+
+
+
+
         list = (ListView) findViewById(R.id.list);
-        list.setAdapter(adapter);
+       // list.setAdapter(adapter);
         list.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(android.widget.AdapterView<?> parent, View view,
                                     int position, long id) {
                 // TODO Auto-generated method stub
-                String Slecteditem = teams[+position];
-                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+                String Slecteditem = teams.get(+position);
+                Toast.makeText(getApplicationContext(), Slecteditem, LENGTH_SHORT).show();
 
 
             }
         });
     }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Firebase teamRef; //Reference to Teams node
+        teamRef=ref.child("Teams");  //Traversing to Teams
+
+        final CustomListView adapter = new CustomListView(this, teams,locations, imgid);
+        //final ArrayAdapter<String> myadapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2,teams,locations);
+        list.setAdapter(adapter);
+
+        teamRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+               // Map<String,String> map=dataSnapshot.getValue(Map.class);
+                teams.add(dataSnapshot.getKey().toString());
+                locations.add(dataSnapshot.child("Location").getValue().toString());
+
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+
+
 }
