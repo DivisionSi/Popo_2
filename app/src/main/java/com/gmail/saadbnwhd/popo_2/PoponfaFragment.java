e4de7a205bc59e0f76750d1999e4debeb5fa2d6e
MusabAkram <musabakram16@gmail.com>
2017-01-22 17:15:52 +0500

    registration form

--- app/src/main/java/com/gmail/saadbnwhd/popo_2/PoponfaFragment.java
+++ app/src/main/java/com/gmail/saadbnwhd/popo_2/PoponfaFragment.java
@@ -1,15 +1,42 @@
 package com.gmail.saadbnwhd.popo_2;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.app.DownloadManager;
+import android.app.DownloadManager.Query;
+import android.app.DownloadManager.Request;
+import android.content.BroadcastReceiver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by Musab on 12/6/2016.
  */
 public class PoponfaFragment extends Fragment {
+  public DownloadManager dm;
     public PoponfaFragment() {
 
     }
@@ -22,6 +49,19 @@ public class PoponfaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_poponfa, container, false);
+        final View view= inflater.inflate(R.layout.fragment_poponfa, container, false);
+        Button form = (Button) view.findViewById(R.id.form);
+        form.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.popofc.com/registration_form.pdf");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    return view;
     }
+
+
+
 }
