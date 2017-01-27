0535f8f3fd7a52993dcedf26fa15623bd853f116
Saad <saadbnwhd@gmail.com>
2017-01-27 22:22:48 +0500

    Edited Results

--- app/src/main/java/com/gmail/saadbnwhd/popo_2/AddFixture.java
+++ app/src/main/java/com/gmail/saadbnwhd/popo_2/AddFixture.java
@@ -14,7 +14,10 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -31,9 +34,11 @@ public class AddFixture extends AppCompatActivity  {
     ListView fixtureteamlist;
     DatePickerDialog.OnDateSetListener date;
     ArrayList<String> TEAMS = new ArrayList<String>(); //String array for Team1 Names
-    ArrayList<String> team2 = new ArrayList<String>(); //String array for Team2 Names
+
     ArrayList<String> locations = new ArrayList<String>();
     ArrayList<String> DateTime=new ArrayList<String>();
+    String AmPm,str_team1,str_team2,str_date,str_time;
+    Integer fixture_id=0;
     Integer[] imgid1 = {
             R.drawable.logo2,
             R.drawable.logo2,
@@ -66,22 +71,46 @@ public class AddFixture extends AppCompatActivity  {
         TIME = (TextView) findViewById(R.id.fix_time);
 
 
-        TEAMS.add("POPO FC");
-        TEAMS.add("REAL AESTADOS FC");
-        TEAMS.add("HALAL MADRID FC");
-        TEAMS.add("ROVERS FC");
-        TEAMS.add("GUNNERS FC");
-        TEAMS.add("SOME TEAM FC");
-        TEAMS.add(":P FC");
-       /* Firebase teamRef; //Reference to Teams node
+        final Firebase FixturesRef=ref.child("League").child("Fixtures");
+
+        FixturesRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                fixture_id++;
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
+       Firebase teamRef; //Reference to Teams node
         teamRef=ref.child("League").child("Teams");  //Traversing to Teams
 
         teamRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 // Map<String,String> map=dataSnapshot.getValue(Map.class);
-                team2.add(dataSnapshot.getKey().toString());
-                team1.add(dataSnapshot.getKey().toString());
+                TEAMS.add(dataSnapshot.getKey().toString());
+
                 locations.add(dataSnapshot.child("Location").getValue().toString());
 
             }
@@ -105,7 +134,7 @@ public class AddFixture extends AppCompatActivity  {
             public void onCancelled(FirebaseError firebaseError) {
 
             }
-        });*/
+        });
 
         date = new DatePickerDialog.OnDateSetListener() {
 
@@ -119,6 +148,7 @@ public class AddFixture extends AppCompatActivity  {
                 String myFormat = "dd/MM/yy"; //In which you need put here
                 SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
                 Date.setText(sdf.format(myCalendar.getTime()));
+                str_date=sdf.format(myCalendar.getTime());
             }
 
         };
@@ -153,6 +183,7 @@ public class AddFixture extends AppCompatActivity  {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         txt_t1.setText(TEAMS.get(i));
+                        str_team1=TEAMS.get(i);
                        // Toast.makeText(AddFixture.this, TEAMS.get(i), Toast.LENGTH_SHORT).show();
                         p.dismiss();
                     }
@@ -176,6 +207,7 @@ public class AddFixture extends AppCompatActivity  {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         txt_t2.setText(TEAMS.get(i));
+                        str_team2=TEAMS.get(i);
                         //Toast.makeText(AddFixture.this, TEAMS.get(i), Toast.LENGTH_SHORT).show();
                         p.dismiss();
                     }
@@ -184,11 +216,20 @@ public class AddFixture extends AppCompatActivity  {
         });
 
 
+
+
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(!txt_t1.getText().equals("Team 1") && !txt_t2.getText().equals("Team 2") && !txt_t1.getText().equals(txt_t2.getText()) && !Date.getText().equals("")
                         && !TIME.getText().equals("")){
+
+
+
+                    FixturesRef.child(Integer.toString(fixture_id)).child("Team1").setValue(str_team1);
+                    FixturesRef.child(Integer.toString(fixture_id)).child("Team2").setValue(str_team2);
+                    FixturesRef.child(Integer.toString(fixture_id)).child("Date").setValue(str_date);
+                    FixturesRef.child(Integer.toString(fixture_id)).child("Time").setValue(str_time);
                         Toast.makeText(AddFixture.this, "Fixture Added", Toast.LENGTH_SHORT).show();
                     }
 
@@ -211,7 +252,7 @@ public class AddFixture extends AppCompatActivity  {
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                         String h = String.valueOf(selectedHour);
                         String m = String.valueOf(selectedMinute);
-                        String state;
+
                        // if(sele)
                         if(h.length()<2)
                         {
@@ -222,7 +263,18 @@ public class AddFixture extends AppCompatActivity  {
                             m = "0"+m;
                         }
 
-                        TIME.setText( h+ " : " + m);
+                        if (selectedHour<12){
+                            AmPm="AM";}
+                        else {
+                            int hr_int;
+                            hr_int=Integer.parseInt(h);
+                            hr_int=hr_int-12;
+                            AmPm = "PM";
+                            h=Integer.toString(hr_int);
+                        }
+
+                        TIME.setText( h+ " : " + m + " " + AmPm);
+                        str_time=h+ ":" + m + " " + AmPm;
                     }
                 }, hour, minute,false);
                 mTimePicker.setTitle("Select Time");
--- app/src/main/java/com/gmail/saadbnwhd/popo_2/MoreFragment.java
+++ app/src/main/java/com/gmail/saadbnwhd/popo_2/MoreFragment.java
@@ -1,11 +1,21 @@
 package com.gmail.saadbnwhd.popo_2;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MoreFragment extends Fragment {
 
     @Override
@@ -14,9 +24,48 @@ public class MoreFragment extends Fragment {
 
     }
 
+    EditText txt_username,txt_password;
+    Button Signin;
+    String username,password;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_more, container, false);
+        View view= inflater.inflate(R.layout.fragment_more, container, false);
+
+        txt_username=(EditText) view.findViewById(R.id.txt_username);
+        txt_password=(EditText) view.findViewById(R.id.txt_password);
+        Signin=(Button) view.findViewById(R.id.btn_signin);
+
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogin();
+            }
+        });
         return view;
     }
+
+    private void userLogin()
+    {
+        String username=txt_username.getText().toString();
+        String password=txt_password.getText().toString();
+
+
+        firebaseAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if ( task.isSuccessful())
+                {
+
+                    Toast.makeText(getActivity(),"Logged in",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
 }
